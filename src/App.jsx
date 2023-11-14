bef70a02a2028db211cdbdd9c98c341b9310b0e2
arvindcoder <kumararvindblr@gmail.com>
2023-11-14 19:31:02 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    if(counter < 100)
+    setCounter(counter + 1);
+  };
+
+  const reduceValue = () => {
+    if(counter > 0)
+    setCounter(counter - 1);
+  };
+
+  return (
+    <>
+      <h1>My First Counter App From  React</h1>
+      <h1>Counter value: {counter}</h1>
+      <button onClick={addValue} >Increment Value</button>
+     
+      <button onClick={reduceValue} className="add">Decrement Value</button>
+    </>
+  );
+}
+
+export default App;
